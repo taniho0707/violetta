7233d461202e24782172dd755da6cb0d6b9d1339
taniho_0707 <taniho_0707@yahoo.co.jp>
2024-09-08 23:56:45 +0900

    環境設定変更のスクリプトを追加

---
+++ Script/switch-config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+	echo "Argument required: Lazuli/LazuliSensor/Zirconia2kai"
+elif [ "$1" = "Lazuli" ]; then
+	echo "Config switched to Lazuli"
+	cp ../.vscode/c_cpp_properties.json.lazuli ../.vscode/c_cpp_properties.json
+	cp ../.vscode/launch.json.lazuli ../.vscode/launch.json
+elif [ "$1" = "LazuliSensor" ]; then
+	echo "Config switched to LazuliSensor"
+	cp ../.vscode/c_cpp_properties.json.lazulisensor ../.vscode/c_cpp_properties.json
+	cp ../.vscode/launch.json.lazulisensor ../.vscode/launch.json
+elif [ "$1" = "Zirconia2kai" ]; then
+	echo "Config switched to Zirconia2kai"
+	cp ../.vscode/c_cpp_properties.json.zirconia2kai ../.vscode/c_cpp_properties.json
+	cp ../.vscode/launch.json.zirconia2kai ../.vscode/launch.json
+else
+	echo "Argument required: Lazuli/LazuliSensor/Zirconia2kai"
+fi
+
